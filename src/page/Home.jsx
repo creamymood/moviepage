a508410326bae7b9ce2ad9c0948048a481680e78
creamymood <wbwbwlgid@naver.com>
2025-05-04 14:03:46 +0900

    인기 영화 api호출 및 맵으로 뿌리기 완료

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react';
-import movieListData  from '../data/movieListData.json';
+import { useEffect, useState } from 'react';
 import MovieCard from '../components/MovidCard';
 import { Link } from 'react-router-dom';
 
 
 function Home() {
-    const [movies, setMovies] = useState(movieListData.results)
+    const [movies, setMovies] = useState([])
+
+    useEffect(()=>{
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDVlYjcyMzNmYmQ1ZDQyMWU1ZGZkODkxMzZjZDIxMiIsIm5iZiI6MTc0NjI1MjE4OS44MzksInN1YiI6IjY4MTViMTlkMGM3ODNiODVmZTkwN2YwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KqsWPPvivWInRUx-3jc9ptyzddgYaKciT7aDe2U7HvI'
+            }
+          };
+          
+          fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+            .then(res => res.json())
+            .then(data => {
+                    console.log(data)
+                    setMovies(data.results)
+
+
+            })
+            .catch(err => console.error(err));
+    },[]);
+    
   
     return (
       <>
